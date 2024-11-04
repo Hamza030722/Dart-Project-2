427c4d8f9cad01a5f7676be508addeadf7875d3b
Hamza030722 <79871560+Hamza030722@users.noreply.github.com>
2024-11-04 23:59:10 +0500

    toDoApp
    toDoApp

--- main.dart
+++ main.dart
@@ -8,79 +8,97 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      color: Colors.blue,
       debugShowCheckedModeBanner: false,
-      title: 'To-Do App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: TodoList(),
+      home: TodoListScreen(),
     );
   }
 }
 
-class TodoList extends StatefulWidget {
+class TodoListScreen extends StatefulWidget {
   @override
-  _TodoListState createState() => _TodoListState();
+  _TodoListScreenState createState() => _TodoListScreenState();
 }
 
-class _TodoListState extends State<TodoList> {
-  final List<String> _tasks = [];
-  final TextEditingController _controller = TextEditingController();
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<String> _todos = [];
 
-  void _addTask() {
-    if (_controller.text.isNotEmpty) {
-      setState(() {
-        _tasks.add(_controller.text);
-        _controller.clear();
-      });
-    }
-  }
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTodo = "";
+        return AlertDialog(
+          backgroundColor: Colors.lime,
+          title: Text("Add your new notes"),
+          content: TextField(
+            onChanged: (value) {
+              newTodo = value;
+            },
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (newTodo.isNotEmpty) {
+                    _todos.add(newTodo);
+                  }
 
-  void _removeTask(int index) {
-    setState(() {
-      _tasks.removeAt(index);
-    });
+                  Navigator.pop(context);
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(
+                    255, 243, 146, 139), // Change to your desired color
+              ),
+              child: Text("Add"),
+            )
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 137, 194, 221),
-        title: Text('To-Do List'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hoverColor: Colors.blue,
-                labelText: 'Add your new task',
-                border: OutlineInputBorder(),
+          title: Text("My Important Notes"), backgroundColor: Colors.lime),
+      body: Container(
+        color: const Color.fromARGB(
+            255, 243, 146, 139), // Change the background color to white
+        child: ListView.builder(
+          itemCount: _todos.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.lime,
+              elevation: 4,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: ListTile(
+                title: Text(
+                  _todos[index],
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors
+                          .black), // Change text color to black for contrast
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete,
+                      color: Colors.red), // Optional: Change delete icon color
+                  onPressed: () {
+                    setState(() {
+                      _todos.removeAt(index);
+                    });
+                  },
+                ),
               ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: _addTask,
-            child: Text('Add Task'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_tasks[index]),
-                  trailing: IconButton(
-                    color: Colors.lightBlue,
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _removeTask(index),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        backgroundColor: Colors.lime,
+        child: Icon(Icons.add),
       ),
     );
   }
